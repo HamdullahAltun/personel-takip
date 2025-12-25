a119122f69f0f6c136feb4904479740136d84117
Hamdullah Altun <hamdullahaltun@Hamdullahs-MacBook-Air.local>
2025-12-26 00:02:38 +0300

    Fix Page Props type for Next.js 15+

--- src/app/(admin)/admin/employees/[id]/page.tsx
+++ src/app/(admin)/admin/employees/[id]/page.tsx
@@ -2,9 +2,9 @@ import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import EmployeeDetailClient from "./client";
 
-export default async function EmployeeDetailPage({ params }: { params: { id: string } }) {
+export default async function EmployeeDetailPage({ params }: { params: Promise<{ id: string }> }) {
     // Await params object before accessing properties
-    const { id } = await Promise.resolve(params);
+    const { id } = await params;
 
     const user = await prisma.user.findUnique({
         where: { id },
